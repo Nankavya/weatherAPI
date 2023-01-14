c7d26b139ce750f55516d4713d829d1522c77534
kavyashree h patwardhan <storeimage@gmail.com>
2023-01-14 23:34:05 +0530

    files added

---
+++ script.js
@@ -0,0 +1,13 @@
+// const apiurl="https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=2d95fa53f174037040153ce5d9da4a89";
+//     function Todos()
+//     {
+//         const apipromise=fetch(apiurl)
+//         console.log('API data',apipromise)
+//         apipromise.then((e)=>{
+//             console.log("data",e)
+//             return e.json()
+//             .then((data)=>{
+//                 console.log("data",data)
+//             })  
+//         }) 
+//     }
